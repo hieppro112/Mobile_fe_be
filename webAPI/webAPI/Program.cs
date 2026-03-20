ada47e146df235b537b916c2bbb547dc859bbbf7
hiep hoc git <lehiep09052005@gmail.com>
2026-03-20 09:21:52 +0700

    edit cs

--- webAPI/webAPI/Program.cs
+++ webAPI/webAPI/Program.cs
@@ -3,38 +3,40 @@ using webAPI.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// ✅ FIX PORT (Railway)
+var port = Environment.GetEnvironmentVariable("PORT");
+if (!string.IsNullOrEmpty(port))
+{
+    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+}
 
-
-// Add services to the container.
-
+// Add services
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<apiDBContext>(option =>
-option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", builder =>
+    options.AddPolicy("AllowAll", policy =>
     {
-        builder.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
     });
 });
-//run deloy
 
 var app = builder.Build();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+// ❌ KHÔNG cần cái này nữa
+// builder.WebHost.UseUrls(...);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// Swagger (luôn bật)
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // app.UseHttpsRedirection();
 
@@ -43,9 +45,5 @@ app.UseCors("AllowAll");
 app.UseAuthorization();
 
 app.MapControllers();
-
-
-app.UseSwagger();
-app.UseSwaggerUI();
 
 app.Run();
